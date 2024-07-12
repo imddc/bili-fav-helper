957140cfe65c01e2ad4665a1bd5de99dcb67897f
Du Cheng <easyhoo2426@163.com>
2024-07-12 11:56:54 +0800

    chore: init

---
+++ src/popup.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import './main.css'
+
+function IndexPopup() {
+  const [data, setData] = useState("111")
+
+  return (
+    <div className="text-red-500">
+      hhh
+    </div>
+  )
+}
+
+export default IndexPopup
