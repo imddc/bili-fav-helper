d5df5de9ef6854bb18710d245e8dc6f8daa70d38
Du Cheng <easyhoo2426@163.com>
2024-07-12 12:55:09 +0800

    fix: content shadcn tailwindcss

--- src/content.tsx
+++ src/content.tsx
@@ -1,6 +1,29 @@
+import cssText from 'data-text:~/main.css'
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
+import { injectMainStyles } from '~/lib/utils'
+
+// 引入tailwind
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
+}
+
 const Content = () => {
+  injectMainStyles(cssText)
+
   return (
-    <div>Content</div>
+    <div className="p-4 space-y-2 bg-sky-50 rounded m-2">
+      <div>
+        <h1>
+          Content
+        </h1>
+      </div>
+
+      <Input />
+      <Button size='sm'>click here</Button>
+    </div>
   )
 }
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const injectMainStyles = (cssText: string) => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  document.head.appendChild(style)
+}
