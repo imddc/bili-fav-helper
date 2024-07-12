2a4a03a87b86998a5b0ac01d58817149695e8b21
Du Cheng <easyhoo2426@163.com>
2024-07-12 12:17:36 +0800

    feat: add shadcn

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react"
+import { Input } from '~/components/ui/input'
 import './main.css'
 
 function IndexPopup() {
   const [data, setData] = useState("111")
 
   return (
-    <div className="text-red-500">
-      hhh
+    <div className="text-red-500 text-2xl w-80 p-4 space-y-2">
+      <div>
+        this is bili fav helper
+      </div>
+
+      <Input placeholder="input here" />
     </div>
   )
 }
