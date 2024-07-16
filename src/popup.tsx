967ad15d83c2eb23481ba8f5bb368198781653f1
Du Cheng <easyhoo2426@163.com>
2024-07-16 12:11:43 +0800

    feat: remove popup content

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,34 +1,7 @@
-import { useState } from "react"
-import { requestBaseDetail } from '~/api'
-import { Input } from '~/components/ui/input'
 import './main.css'
 
-chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-  sendResponse('我收到了你的来信')
-  console.log('接收了来自 content.js的消息', req.info)
-})
-
-
-async function getBVDetail() {
-  const res = await requestBaseDetail('BV1Sb4y1X7p7')
-  console.log(res, 'res')
-}
-
-
 function IndexPopup() {
-  const [data, setData] = useState("111")
-
-  getBVDetail()
-
-  return (
-    <div className="text-red-500 text-2xl w-80 p-4 space-y-2">
-      <div>
-        this is bili fav helper
-      </div>
-
-      <Input placeholder="input here" />
-    </div>
-  )
+  return null
 }
 
 export default IndexPopup
