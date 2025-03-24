e8c3e6a16b04f73268f0fe900be375514e7f13da
imddc <easyhoo2426@163.com>
2025-03-24 12:36:53 +0800

    lint

--- src/content.tsx
+++ src/content.tsx
@@ -1,25 +1,19 @@
 import cssText from 'data-text:~/main.css'
-import type { PlasmoCSConfig } from "plasmo"
+import type { PlasmoCSConfig } from 'plasmo'
 import HoverButton from '~/components/hover-button'
 import { useVideoControl } from '~/hooks/use-video-control'
 import { getLastPath, injectMainStyles } from '~/lib/utils'
 
 export const config: PlasmoCSConfig = {
-  matches: ["<all_urls>"],
+  matches: ['<all_urls>'],
   all_frames: true
 }
 
 // 引入tailwind
 export const getStyle = () => {
-  const style = document.createElement("style")
+  const style = document.createElement('style')
   style.textContent = cssText
   return style
-}
-
-function getLastPath() {
-  const pathname = window.location.pathname
-  const lastPath = pathname.split('/').filter(Boolean).pop()
-  return lastPath
 }
 
 function isBv(path: string) {
